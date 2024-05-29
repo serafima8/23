1339d85fe6d8b62b47b6f061eb87ebeb2dcdce2a
serafima8 <147659413+serafima8@users.noreply.github.com>
2024-05-29 23:44:02 +0300

    Add files via upload

---
+++ Задача 4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Role role = new Role();
+            foreach (var elem in Enum.GetValues(typeof(Role.UserRole)))
+            {
+                Console.WriteLine(elem);
+            }
+            Console.Write("Введите роль пользователя: ");
+            string user = Console.ReadLine();
+            role.Roles(user);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Задача 4/Role.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_4
+{
+    class Role
+    {
+        public enum UserRole
+        {
+            Администратор,
+            Модератор,
+            Пользователь,
+            Гость
+        }
+        public void Roles(string user)
+        {
+            {
+                switch (user.ToLower())
+                {
+                    case "администратор":
+                        Console.WriteLine($"Вы зарегестрировны как {UserRole.Администратор}");
+                        break;
+                    case "модератор":
+                        Console.WriteLine($"Вы зарегестрировны как {UserRole.Модератор}");
+                        break;
+                    case "пользователь":
+                        Console.WriteLine($"Вы зарегестрировны как {UserRole.Пользователь}");
+                        break;
+                    case "гость":
+                        Console.WriteLine($"Вы зарегестрировны как {UserRole.Гость}");
+                        break;
+                }
+            }
+        }
+    }
+}
